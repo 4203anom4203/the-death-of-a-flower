3da62c33d962754c312baddfb683ff629514705e
4203anom4203 <4203anom4203@gmail.com>
2026-05-27 13:23:34 -0400

    Add second camera
    for characters only, i did this without a linter so idk whats going to work

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,11 @@ use bevy::{
 fn main() -> AppExit {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, setup_camera)
+        .add_systems(Startup, setup_cameras)
         .run()
 }
 
-fn setup_camera(mut commands: Commands) {
+fn setup_cameras(mut commands: Commands) {
     //higher render layer = on top :)
 
     commands.spawn((
@@ -27,6 +27,20 @@ fn setup_camera(mut commands: Commands) {
         //can only see things that are on the layer 0, otherwise it won't render.
     ));
 
+    commands.spawn((
+        //Second camera for character sprites
+        Camera2d,
+        Char,
+        Camera {
+            //again for characters and stuff they hold etc.
+            order: 1,
+            clear_color: ClearColorConfig::Custom(Color::NONE),
+            ..default()
+        },
+
+        RenderLayers::layer(1)
+    ));
+
 
 }
 
@@ -34,4 +48,5 @@ fn setup_camera(mut commands: Commands) {
 #[derive(Component)]
 struct Bg;
 //we will throw any backgrounds onto things with this component, aka the first camera
-//
+#[derive(Component)]
+struct Char;
