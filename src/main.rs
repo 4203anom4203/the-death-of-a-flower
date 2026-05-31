bef40037d524f82d183964436c16d10711ef0238
Anøm <4203anom4203@gmail.com>
2026-05-31 11:41:14 -0400

    settings shaded overlay

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,5 @@
 use bevy::{
-    camera::visibility::RenderLayers,
-    color::palettes::css::{BLACK, RED, YELLOW, GREEN}, //Don't mind these its for debug
-    input_focus::InputFocus,
-    window::Window,
-    window::WindowMode,
-    prelude::*
+    camera::visibility::RenderLayers, color::palettes::css::{BLACK, GREEN, RED, YELLOW}, image::DataFormat::Rgba, input_focus::InputFocus, prelude::*, window::{Window, WindowMode}
 };
 
 
@@ -12,10 +7,13 @@ fn main() -> AppExit {
     App::new()
         .add_plugins(DefaultPlugins)
         .init_resource::<InputFocus>()
+        .init_resource::<MenuState>()
         .add_systems(Startup, setup)
-        .add_systems(Update, button_system)
+        .add_systems(Update, settings_button_system)
+        .add_systems(Update, update_settings_menu)
         .run()
 }
+
 
 //setup func is for setting up title screen, everything else can move after
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>, mut window: Single<&mut Window>) {
@@ -29,14 +27,13 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>, mut window: Sin
         //first camera/bg camera
         Camera2d,
         Camera {
-            //this is for background art, idk we need a resources folder
+            //This camera is for bg/sprites/every non interact item
             order: 0,
             clear_color: ClearColorConfig::Custom(Color::Srgba(BLACK)),
             //cursed as hell syntax
             ..default()
         },
         RenderLayers::layer(0)
-        //Renderlayer is 0, its the default, make sure to match renderlayers
         
     ));
     //we lowk may need more, background, character, text, buttons thats 4 so far, good enough
@@ -53,48 +50,14 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>, mut window: Sin
         RenderLayers::layer(1)
     ));
 
-    commands.spawn((
-        //third camera for text box
-        Camera2d,
-        Camera {
-            //textboxes etc
-            order: 2,
-            clear_color: ClearColorConfig::None,
-            ..default()
-        },
-
-        RenderLayers::layer(2)
-    ));
-    
-    commands.spawn((
-        //last camera hopefully for ui/overlay and buttons
-        Camera2d,
-        Camera {
-            order: 3,
-            clear_color: ClearColorConfig::None,
-            ..default()
-        },
-        RenderLayers::layer(3)
-    ));
-    //we need another one for the settings actual window/popup
-
-    commands.spawn((
-        Camera2d, 
-        Camera {
-            order: 4,
-            clear_color: ClearColorConfig::None,
-            ..default()
-        },
-
-        RenderLayers::layer(4)
-    ));    
     //image spawning
     commands.spawn((
+        ZIndex(0), //bg
         Sprite {
             image: asset_server.load("placeholder.png"),
             ..default()
         },
-        RenderLayers::layer(0)
+        
         //default renderlayer is 0
     ));
     //alr, this the settings icon.
@@ -104,6 +67,7 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>, mut window: Sin
         Button,
         //BorderColor::all(RED),
         SettingsButton,
+        ZIndex(5), //buttons
         Node {
             position_type: PositionType::Absolute,
             width: Val::Px(50.0),
@@ -130,15 +94,28 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>, mut window: Sin
             }
             
         )],
-        RenderLayers::layer(3)
         
         //i almost forgot this
     ));
 
+    commands.spawn((
+        SettingsPanel,
+        BackgroundColor(Color::srgba(0.0, 0.0, 0.0, 0.8)),
+        Visibility::Hidden,
+        Node {
+            width: Val::Percent(100.0),
+            height: Val::Percent(100.0),
+            justify_content: JustifyContent::Center,
+            align_items: AlignItems::Center,
+            ..default()
+        }
+    ));
+
 }
 
-fn button_system (
+fn settings_button_system (
     mut input_focus: ResMut<InputFocus>,
+    mut state: ResMut<MenuState>,
     mut interaction_query: Query<(Entity, &Interaction, /* &mut BorderColor,*/ &Button, &SettingsButton), Changed<Interaction>>,
 ) {
     for (entity, interaction, /*mut border_color,*/ &Button, &SettingsButton) in &mut interaction_query {
@@ -146,6 +123,10 @@ fn button_system (
             Interaction::Pressed => {
                 input_focus.set(entity);
                 //*border_color = BorderColor::all(GREEN);
+                state.current_menu = match state.current_menu {
+                Menu::None => Menu::Settings,
+                Menu::Settings => Menu::None, //settings overlay tobble
+};
             }
 
             Interaction::Hovered => {
@@ -160,19 +141,39 @@ fn button_system (
         }
     }
 }
+
+fn update_settings_menu (
+    menu: ResMut<MenuState>,
+    mut panel_query: Query<&mut Visibility, With<SettingsPanel>>,
+) {
+    for mut visibility in &mut panel_query {
+        *visibility = if menu.current_menu == Menu::Settings {
+            Visibility::Visible
+        } else {
+            Visibility::Hidden
+        };
+    }
+    
+}
+
+
+#[derive(Default, PartialEq)]
+enum Menu {
+    #[default]
+    None,
+    Settings,
+    //can add shit
+}
+//resources
+#[derive(Resource, Default)]
+struct MenuState {
+    current_menu: Menu,
+}
+
 //components
 #[derive(Component)]
 struct SettingsButton;
 
-/*
-use 
 #[derive(Component)]
-enum MenuButton {
-    Settings,
-    Save,
-    Load,
-    Quit,
-}
-
-    when we add more than one button
-*/
+struct SettingsPanel;
+//my linter blew up because of unused code
